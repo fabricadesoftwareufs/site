07aa3a7373b971dd768c4cc8278f7c71afc3c338
João Alves <joaoalves.cs@gmail.com>
2020-06-23 00:08:46 -0300

    Algumas alterações feitas no estilo da página.

--- _site/assets/js/app.js
+++ _site/assets/js/app.js
@@ -1,9 +1,10 @@
 const postsContainer = document.querySelector('#posts-container')
+const loaderContainer = document.querySelector('.loader')
 
 let page = 1
 
 const getPosts = async () => {
-    const response = await fetch(`/site/api-post.json?_limit=3&_page=${page}`)
+    const response = await fetch(`/site/api-post.json?_limit=1&_page=${page}`)
     return response.json()
 }
 
@@ -11,7 +12,8 @@ const addPostsIntoDOM = async() => {
     const posts = await getPosts()
     const postsTemplate = posts.map(({number, title, url, description}) => `
         <h2> <a href=${url}> ${title} </a> </h2>
-        <p>${description}</p>
+        <h1>${description}</h1>
+        <h1 class="button special"> <a href=${url}> LEIA MAIS </a><h1>
     `).join('')
 
     postsContainer.innerHTML += postsTemplate
@@ -19,5 +21,30 @@ const addPostsIntoDOM = async() => {
 
 addPostsIntoDOM()
 
+const getNextPosts = () => {
+    page++
+    addPostsIntoDOM()
+}
 
+
+const removeLoader = () => {
+    setTimeout(() => {
+        loaderContainer.classList.remove('show')
+        getNextPosts()
+    }, 1000)
+}
+
+const showLoader = () => {
+    loaderContainer.classList.add('show')
+    removeLoader()
+}
+
+window.addEventListener('scroll', () => {
+    const { clientHeight, scrollHeight, scrollTop } = document.documentElement
+    const isPageBottomAlmostReached = scrollTop + clientHeight >= scrollHeight - 10
+
+    if (isPageBottomAlmostReached){
+        showLoader()
+    }
+})
 /* O vídeo acaba em 22:48 -- */
